5b64421595801f4ca7446316c8bc3e13d984e7e7
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-01-08 11:46:52 -0300

    hasdasuh

---
+++ README.md
@@ -0,0 +1,9 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# WordCloudFromPapers
+
+<figure>
+<img src="wc.png" alt="Example" />
+<figcaption aria-hidden="true">Example</figcaption>
+</figure>
--- Scripts/wordcloud.R
+++ Scripts/wordcloud.R
@@ -1,7 +1,27 @@
-
+library(webshot)
+library(htmlwidgets)
+webshot::install_phantomjs()
 set.seed(42)
 
+
+tiff(filename = paste0("papers",Sys.Date(),".tif"),
+     width = 450,height = 450,res = 600)
 wordcloud2(freq_tokens,
            size = .75,
            minRotation = -pi/2, maxRotation = -pi/2)
- 
+dev.off() 
+
+(
+hw <- wordcloud2(freq_tokens,
+                 size = .75,
+                 minRotation = -pi/2, maxRotation = -pi/2)
+
+)
+saveWidget(hw,"1.html",selfcontained = F)
+
+webshot::webshot("1.html",
+                 file = "wc.png",
+                 vwidth = 1000,
+                 vheight = 800,
+                 delay =30,zoom = 10)
+
